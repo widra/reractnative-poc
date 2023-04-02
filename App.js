5b093a8ca1c8de071ecbf3893cdf26d442bc2ff7
Peter Magyar <magyar.peter1@gmail.com>
2023-04-02 20:01:56 +0200

    feat: react navigation stack added

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-
-
-export default function App() {
-
-
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-
-
-    
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  console.log("App.js: App() called");
+  return (
+    <NavigationContainer>
+      <HomeScreen />
+    </NavigationContainer>
+  );
+}
